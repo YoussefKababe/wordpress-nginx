59bd04e05e5accefb2aec87091f0d81f263a66e4
Youssef Kababe <hello@youssefkababe.com>
2015-12-29 17:40:28 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,11 @@
 
 rsync -aq wpc/* wp-content
 chown -R www-data:www-data wp-content
+
+if [ -f "wp-config.php" ]
+then
+  sed  "/require_once(ABSPATH . 'wp-settings.php');/ { N; s/require_once(ABSPATH . 'wp-settings.php');/if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') $_SERVER['HTTPS']='on';\n&/ }" wp-config.php
+fi
+
 service php5-fpm start
 nginx
